b4ac94ab0bc627963ba607d365aa758989ef5542
timheuer <timheuer@THRILLA>
2016-12-12 22:50:00 -0800

    Add project files.

---
+++ NumberFunctions/Function.cs
@@ -0,0 +1,61 @@
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using Slight.Alexa.Framework.Models.Requests;
+using Slight.Alexa.Framework.Models.Responses;
+using System;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace NumberFunctions
+{
+    public class Function
+    {
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public string FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            Response response;
+            IOutputSpeech innerResponse = null;
+            var log = context.Logger;
+
+            if (input.GetRequestType() == typeof(Slight.Alexa.Framework.Models.Requests.RequestTypes.ILaunchRequest))
+            {
+                // default launch request, let's just let them know what you can do
+                log.LogLine($"Default LaunchRequest made");
+
+                innerResponse = new PlainTextOutputSpeech();
+                (innerResponse as PlainTextOutputSpeech).Text = "Welcome to number functions.  You can ask us to add numbers!";
+            }
+            else if (input.GetRequestType() == typeof(Slight.Alexa.Framework.Models.Requests.RequestTypes.IIntentRequest))
+            {
+                // intent request, process the intent
+                log.LogLine($"Intent Requested {input.Request.Intent.Name}");
+
+                // AddNumbersIntent
+                // get the slots
+                var n1 = Convert.ToDouble(input.Request.Intent.Slots["firstnum"].Value);
+                var n2 = Convert.ToDouble(input.Request.Intent.Slots["secondnum"].Value);
+
+                double result = n1 + n2;
+
+                innerResponse = new PlainTextOutputSpeech();
+                (innerResponse as PlainTextOutputSpeech).Text = $"The result is {result.ToString()}.";
+
+            }
+
+            response = new Response();
+            response.ShouldEndSession = true;
+            response.OutputSpeech = innerResponse;
+            SkillResponse skillResponse = new SkillResponse();
+            skillResponse.Response = response;
+            skillResponse.Version = "1.0";
+
+            return JsonConvert.SerializeObject(skillResponse);
+        }
+    }
+}
